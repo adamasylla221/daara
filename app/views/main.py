61c9f98dd3f0b48f3993190e2548e33957fc8f74
DELL <adamssylla221@gmail.com>
2026-07-17 20:14:01 +0000

    feat: tableau de bord avec statistiques et dernieres progressions

--- app/views/main.py
+++ app/views/main.py
@@ -1,8 +1,30 @@
 from flask import Blueprint, render_template
 
+from app.models.maitre import Maitre
+from app.models.classe import Classe
+from app.models.talibe import Talibe
+from app.models.progression import Progression
+
 bp_main = Blueprint("main", __name__)
 
 
 @bp_main.route("/")
-def accueil():
-    return render_template("main/index.html")
+def accueil() -> str:
+    nb_maitres = Maitre.query.count()
+    nb_classes = Classe.query.count()
+    nb_talibes = Talibe.query.count()
+
+    dernieres_progressions = (
+        Progression.query
+        .order_by(Progression.date_evaluation.desc())
+        .limit(10)
+        .all()
+    )
+
+    return render_template(
+        "main/index.html",
+        nb_maitres=nb_maitres,
+        nb_classes=nb_classes,
+        nb_talibes=nb_talibes,
+        progressions=dernieres_progressions,
+    )
